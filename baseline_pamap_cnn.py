9a6c408dec4edbdb4fc00c0d00422b5277f3cb35
tengqi159 <48057345+tengqi159@users.noreply.github.com>
2020-04-26 09:25:54 +0800

    Update baseline_pamap_cnn.py
    baseline_pamap_cnn

--- baseline_pamap_cnn.py
+++ baseline_pamap_cnn.py
@@ -29,95 +29,18 @@ print(n_gpu)
 path=os.path.dirname(os.path.abspath("__file__"))
 print(path)
 
-# pathlist = [r'./oppotunity_sum/pamap2_12/X_train.npy',
-#             r'./oppotunity_sum/pamap2_12/y_train.npy',
-#             r'./oppotunity_sum/pamap2_12/X_test.npy',
-#             r'./oppotunity_sum/pamap2_12/y_test.npy']
-
-
 
 pathlist = [r'./oppotunity_sum/pamap_new/train_X_new.npy',
             r'./oppotunity_sum/pamap_new/train_y_new.npy',
             r'./oppotunity_sum/pamap_new/total_pamap2_valtestx.npy',
             r'./oppotunity_sum/pamap_new/total_pamap2_valtesty.npy']
-# #
-#
-# pathlist = [r'./oppotunity_sum/pamap2_all/train_x_split_all.npy',
-#             r'./oppotunity_sum/pamap2_all/train_y_split_all.npy',
-#             r'./oppotunity_sum/pamap2_all/test_x_split_all.npy',
-#             r'./oppotunity_sum/pamap2_all/test_y_split_all.npy']#(4584, 171, 40) (1965, 171, 40) (4584,) (1965,)
+
 
 train_x=np.load(pathlist[0])
 train_y=np.load(pathlist[1])
 test_x=np.load(pathlist[2])
 test_y=np.load(pathlist[3])
 print(train_x.shape,train_y.shape,test_x.shape,test_y.shape)
-
-#
-#
-# # this is UCIdataset. torch.Size([7352,128, 9]) torch.Size([7352]) windows size:128 channel:9 calss:6 overlap:50%
-# #
-# X_train=np.load(pathlist[0])
-# X_test=np.load(pathlist[2])
-# #
-# # print(X_test.shape)
-# X_train_copy=X_train
-# # print(X_train_copy,'ppppppppppppp')
-# X_test_copy=X_test
-# # print(X_train_copy,'=============================================================\n')
-# def normalization(data):
-#     shape=data.shape
-#     for i in range(data.shape[0]):
-#         for j in range(data.shape[2]):
-#             data[i, :, j] = (data[i, :, j] - np.mean(data[i, :, j])) / np.std(data[i, :, j])
-#     return data
-# a=normalization(X_train_copy)
-# b=normalization(X_test_copy)
-#[][][][]
-# np.save('./oppotunity_sum/pamap2_all/X_train_normalization.npy',a)
-# np.save('./oppotunity_sum/pamap2_all/X_test_normalization.npy',b)
-# def downsampled(data):
-#     shape=data.shape
-#     for i, _, in enumerate(range(shape[0])):
-#         if i==0:
-#             for j ,count, in enumerate(range(shape[1])):
-#                 if j==0:
-#                     print(i, j)
-#                     zero=data[i,j,:]
-#                     # print(zero)
-#                 elif j%3==0:
-#                     print(i,j)
-#                     init = data[i,j,:]
-#                     zero=np.vstack((zero,init))
-#             zero=np.reshape(zero,[1,171,9])
-#             # print(zero.shape)
-#         else:
-#             for j ,count, in enumerate(range(shape[1])):
-#                 if j==0:
-#                     c=data[i,j,:]
-#                 elif j%3==0:
-#                     init = data[i,j,:]
-#                     c=np.vstack((c,init))
-#             c = np.reshape(c, [-1, 171, 9])
-#         if i==0:
-#             down=zero
-#         else :
-#             # print(c.shape, 'c.shape')
-#             down=np.vstack((down,c))
-#     return down
-#
-# X_train_copy=downsampled(X_train_copy)
-# X_test_copy=downsampled(X_test_copy)
-# print(X_train_copy.shape,X_test_copy.shape)
-# np.save('./oppotunity_sum/pamap2_all/X_train_normalization_downsample.npy',X_train_copy)
-# np.save('./oppotunity_sum/pamap2_all/X_test_normalization_downsample.npy' ,X_test_copy)
-# trainx=np.load(pathlist[0])
-# trainy=np.load(pathlist[1]).shape
-# print(trainx,trainy)
-
-
-
-#
 
 # @torchsnooper.snoop()
 def data_flat(data_y):
@@ -135,7 +58,7 @@ def load_data(path_X,path_y,batchsize):
     train_y = np.load(path_y)
     train_y = torch.from_numpy(train_y).cuda()
 
-    print(train_x.shape,train_y.shape,'ppppppppppppppppppp')
+    #print('The train_x shape:',train_x.shape,'The train_y shape:',train_y.shape)
     torch_dataset=Data.TensorDataset(train_x,train_y)
     loader=Data.DataLoader(
         dataset=torch_dataset,
@@ -144,24 +67,9 @@ def load_data(path_X,path_y,batchsize):
         num_workers=0,
     )
     total=len(loader)
-    # for _ in tqdm(range(total), desc='进行中', ncols=80,postfix="train_data"):
+    # for _ in tqdm(range(total), desc='Ongoing', ncols=80,postfix="train_data"):
     #     pass
     return loader
-
-def similarity_matrix(x):
-    ''' Calculate adjusted cosine similarity matrix of size x.size(0) x x.size(0). '''
-    if x.dim() == 4:
-        if x.size(1) > 3 and x.size(2) > 1:
-            z = x.view(x.size(0), x.size(1), -1)
-            x = z.std(dim=2)
-            # print('this similarity matrix x shape',x.shape)
-        else:
-            x = x.view(x.size(0), -1)
-    xc = x - x.mean(dim=1).unsqueeze(1)
-    xn = xc / (1e-8 + torch.sqrt(torch.sum(xc ** 2, dim=1))).unsqueeze(1)
-    R = xn.matmul(xn.transpose(1, 0)).clamp(-1, 1)
-    # print('this similarity matrix x shape\n', R.shape)
-    return R
 
 def quzheng_x(height,kernel_size,padding,stride,numlayer):
     list=[]
@@ -178,14 +86,6 @@ def quzheng_s(height,kernel_size,padding,stride,numlayer):
         list.append(feature)
     return list
 
-class Loss(nn.Module):
-    def __init__(self):
-        super(Loss, self).__init__()
-        print('this is loss function!')
-
-    def forward(self, output, label):
-        loss_func = F.cross_entropy(output, label)
-        return loss_func
 class cnn(nn.Module):
     def __init__(self):
         super(cnn, self).__init__()
@@ -220,8 +120,8 @@ class cnn(nn.Module):
 
 
 
-        self.linear = nn.Linear(56832, 1024)
-        self.linear1 = nn.Linear(1024, 12)
+        self.linear = nn.Linear(56832, 12)
+
         # self.soft=nn.Softmax(1)
 
 
@@ -244,8 +144,6 @@ class cnn(nn.Module):
         x = self.linear(x)
         # x = self.soft(x)
 
-        # x = F.softmax(x,dim=1)
-
         return x
 
 
@@ -261,7 +159,7 @@ def plot_confusion(comfusion,class_data):
     plt.figure(figsize=(12,9))
     plt.rcParams['font.family'] = ['Times New Roman']
     classes = class_data
-    plt.imshow(comfusion, interpolation='nearest', cmap=plt.cm.Oranges)  # 按照像素显示出矩阵
+    plt.imshow(comfusion, interpolation='nearest', cmap=plt.cm.Oranges)  
     plt.title('confusion_matrix',fontsize = 12)
     plt.colorbar()
     tick_marks = np.arange(len(classes))
@@ -270,10 +168,10 @@ def plot_confusion(comfusion,class_data):
     plt.tick_params(labelsize=12)
     thresh = comfusion.max() / 2.
     # iters = [[i,j] for i in range(len(classes)) for j in range((classes))]
-    # ij配对，遍历矩阵迭代器
+    
     iters = np.reshape([[[i, j] for j in range(len(classes))] for i in range(len(classes))], (comfusion.size, 2))
     for i, j in iters:
-        plt.text(j, i, format(comfusion[i, j]))  # 显示对应的数字
+        plt.text(j, i, format(comfusion[i, j]))  
 
     plt.ylabel('Real label',fontsize = 12)
     plt.xlabel('Prediction',fontsize = 12)
@@ -291,37 +189,19 @@ def train(train_loader, test_x_path, test_y_path,test_error,optim):
 
     for step, (batch_x, batch_y) in enumerate(train_loader):
         batch_x, batch_y = batch_x.cuda(), batch_y.cuda()
+
         target_onehot = to_one_hot(batch_y)
         target_onehot = target_onehot.cuda()
-        # print(batch_x.shape,target_onehot.shape,batch_y.shape,'batch_x,target_onehot.shape,batch_y.shape')
-
-        # check_parameters(model,2)
-
 
         output = model(batch_x)
-        # print(output.shape,batch_y.shape,target_onehot.shape,'output.shape')
+
         optimizer.zero_grad()
         loss = loss_func(output, batch_y.long())
 
-        # check_parameters(model, 2)
-
-        # loss_t.backward()
         loss.backward()
-
         optimizer.step()
-        # sch.step()
 
-            # if epoch%10==0:
-            # print('局部更新')
-            # check_parameters(model, 2)
-        # check_parameters(model, 16)
-        # params = list(model.named_parameters())
-        # (name, param) = params[11]
-        # print('___________________________________________________________________\n', name, param,
-        #       '\n____________________________________________________________________')
-        # train_output = torch.max(output, 1)[1].cuda()
-        # taccuracy = (torch.sum(train_output == batch_y.long()).type(torch.FloatTensor) / batch_y.size(0)).cuda()
-        # print(taccuracy,'train_accuracy')
+
     if epoch % 1 == 0:
         model.eval()
         #
@@ -339,28 +219,29 @@ def train(train_loader, test_x_path, test_y_path,test_error,optim):
         try:
             test_output= model(test_x)
             test_output_copy=test_output
-            # print(test_output.shape)
+
             test_output=data_flat(test_output.cpu().detach().numpy())
-            # print(test_output.shape, 'test_output')
 
             test_output_f1 =np.asarray(pd.get_dummies(test_output))
 
             print(test_y_onehot.shape, test_output_f1.shape)
+            
             acc = accuracy_score(test_y_onehot.cpu().numpy(), test_output_f1)
             f1 = f1_score(test_y_onehot.cpu().numpy(), test_output_f1, average='weighted')
             f2 = f1_score(test_y_onehot.cpu().numpy(), test_output_f1, average='micro')
             f3 = f1_score(test_y_onehot.cpu().numpy(), test_output_f1, average='macro')
             reca = recall_score(test_y.cpu().numpy(), test_output,average='weighted')
+            
             if epoch %2==0:
                 confusion = sm.confusion_matrix(test_y.cpu().numpy(), test_output)
-                print('混淆矩阵为：', confusion, sep='\n')
+                print('The confusion matrix is ：', confusion, sep='\n')
                 plot_confusion(confusion,
                                ['Lying', 'Sitting', 'Standing', 'Walking', 'Running', 'Cycling', 'Nordic walking',
                                 'Ascending stairs', 'Descending stairs', 'Vacuum cleaning', 'Ironing', 'Rope jumping'])
 
             # print(confusion_y.tolist(), '\n', test_output_f1_con.tolist())
             print('Epoch: ', epoch, '| test accuracy: %.8f' % acc, '| test F1: %.8f' % f1, '| test recall: %.8f' % reca, '| test micro: %.8f' % f2, '| test micro: %.8f' % f3)
-        #
+        
             test_output = torch.max(test_output_copy, 1)[1].cuda()
             # print(test_output.shape,'test_output.shape')
             accuracy = (torch.sum(test_output == test_y.long()).type(torch.FloatTensor) / test_y.size(0)).cuda()
@@ -369,7 +250,7 @@ def train(train_loader, test_x_path, test_y_path,test_error,optim):
             model.train()
         except ValueError:
             # confusion = sm.confusion_matrix(test_y.cpu().numpy(), test_y.cpu().numpy())
-            # print('混淆矩阵为：', confusion, sep='\n')
+            # print('The confusion matrix is ：', confusion, sep='\n')
             # plot_confusion(confusion,['lying','sitting','standing','walking','running','cycling','Nordic walking','ascending stairs','descending stairs','vacuum cleaning','ironing','other'])
             print('error')
         else:
@@ -389,7 +270,7 @@ if __name__ == '__main__':
     loss_func = nn.CrossEntropyLoss().cuda()
     train_loader = load_data(pathlist[0], pathlist[1], batchsize=300)
     test_error = []
-    for epoch in range(201):
+    for epoch in range(200):
 
         train(train_loader, pathlist[2], pathlist[3],test_error,optimizer)
 
