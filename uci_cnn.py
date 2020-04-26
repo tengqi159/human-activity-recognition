f37318a9e3fa16ba526aa40da38b26a3ae63ab6b
tengqi159 <48057345+tengqi159@users.noreply.github.com>
2020-04-26 16:16:34 +0800

    Update uci_cnn.py

--- uci_cnn.py
+++ uci_cnn.py
@@ -116,21 +116,13 @@ class conv_loss_block(nn.Module):
         self.decode_ys = []
         self.bns_decode_ys = []
 
-
-        decode_t_list = [15872, 15360, 14848, 14336]  # pool_size[2,2]
-        decode_t_list = [31744, 28672, 24576, 35840]#pool_size[1,2]
-        decode_t_list = [46848, 28672, 12288, 35840]  # pool_size[1,2]
-        decode_t_list = [7680, 3072, 3072, 35840]  # pool_size[1,2]
-        decode_t_list = [30720, 12288, 3072, 35840]  # pool_size[1,2]
-        decode_t_list = [29568, 21632, 15360, 35840]  # pool_size[1,2]
-        # print(int(channel_out*height_width*0.5),'self.biasself.biasself.biasself.biasself.bias')
-        # print(channel_in, channel_out,'channel_in, channel_out, height_width, kernel, stride, bias')
+        decode_t_list = [29568, 21632, 15360, 35840]  
 
         self.encoder =nn.Sequential(nn.Conv2d(self.channel_in, self.channel_out, (6, 1),stride=stride,padding=1),
                                     nn.BatchNorm2d(self.channel_out),
                                     nn.ReLU(inplace=True),
-
                                     )
+
         self.avg_pool = nn.MaxPool2d((2, 2),stride=1)
 
         for i in range(3):
@@ -138,10 +130,6 @@ class conv_loss_block(nn.Module):
             setattr(self, 'decode_y%i' % i, decode_y)
             self._set_init(decode_y)
             self.decode_ys.append(decode_y)
-
-        # self.decode_y = nn.Linear(4096, self.num_class)
-        # # print(channel_out * height_width,'channel_out * height_width')
-        # self.decode_y.weight.data.zero_()
 
         self.conv_loss = nn.Conv2d(self.channel_out, self.channel_out, kernel_size=(3, 3), stride=(1, 1),
                                    padding=(1, 1), bias=False)
@@ -167,15 +155,6 @@ class conv_loss_block(nn.Module):
         self.correct = 0
         self.examples = 0
 
-    def print_stats(self):
-        stats = '{}, loss_sim={:.4f}, loss_pred={:.4f}, error={:.3f}%, num_examples={}\n'.format(
-            self.encoder,
-            self.loss_sim / self.examples,
-            self.loss_pred / self.examples,
-            100.0 * float(self.examples - self.correct) / self.examples,
-            self.examples)
-        return stats
-
     def set_learning_rate(self, lr):
         self.lr = lr
 
@@ -186,49 +165,41 @@ class conv_loss_block(nn.Module):
         self.optimizer.zero_grad()
 
     def optim_step(self):
-        # print('下一步优化网络')
         self.optimizer.step()
 
     def forward(self, x, y, y_onehot, loop, is_training):
-        # print(x.shape,'xxxxxxxxxxxx')
-        h = self.encoder(x)
-        # print(h.shape,'h.shapeh.shapeh.shapeh.shapeh.shapeh.shapeh.shapeh.shapeh.shapeh.shapeh.shapeh.shapeh.shapeh.shapeh.shape')
 
+        h = self.encoder(x)
 
         h_return = h
         h_shape = h.shape
-        # print(h.shape,'h.shapeh.shapeh.shapeh.shapeh.shapeh.shape')
+
         h_return = self.dropout(h_return)
 
-        #####先省略pooling#######
+
         h_loss = self.conv_loss(h_return)
-        # print(h_loss.shape,y_onehot.shape,'h_loss.shape,y_onehot.shape')
+
         Rh = similarity_matrix(h_loss)
 
         # caculate unsupervised loss
         Rx = similarity_matrix(x).detach()
         loss_unsup = F.mse_loss(Rh, Rx)
 
-        # h_pool = self.avg_pool(h_return)iiiij
         h_pool = h_return
-        # print(h_pool.shape,'h_pool.view(h_pool.size(0)')
 
         y_hat_local = self.decode_ys[loop](h_pool.view(h_pool.size(0), -1))
-        # print(y_hat_local.shape, y.shape,y_onehot.shape,'y_hat_local.shape, y.detach().shape')
         loss_pred = (1 - 0.99) * F.cross_entropy(y_hat_local, y.detach().long())
 
         Ry = similarity_matrix(y_onehot).detach()
         loss_sim = 0.99* F.mse_loss(Rh, Ry)
 
-        # print(loss_sim,'loss_simloss_simloss_simloss_simloss_simloss_simloss_simloss_sim')
         loss_sup = loss_pred + loss_sim
-        # loss = Variable(loss_pred+loss_sim, requires_grad=True)
 
         loss = loss_sup * 1 + loss_unsup * 0
 
         if is_training:
-            # print(self.training,'局部反向传播')
             loss.backward(retain_graph=False)
+            
         if is_training:
             self.optimizer.step()
             self.optimizer.zero_grad()
@@ -253,19 +224,7 @@ class convnet(nn.Module):
              ])
 
         self.layer_out = nn.Linear(15360, num_classes)
-
         self.layer_out.weight.data.zero_()
-        # self.layer_out = nn.Sequential(
-        #                                nn.Linear(64512, 128,bias=True),
-        #                                nn.Dropout2d(0.5),
-        #                                nn.ReLU(),
-        #                                nn.Linear(128, 128, bias=True),
-        #                                nn.Dropout2d(0.5),
-        #                                nn.ReLU(),
-        #                                nn.Linear(128,6,bias=True)
-        # )
-
-        # self.layer_out.weight.data.zero_()
 
         bn = nn.BatchNorm2d(1, momentum=0.5)
         setattr(self, 'pre_bn', bn)
@@ -289,22 +248,19 @@ class convnet(nn.Module):
             layer.optim_zero_grad()
 
     def forward(self, x, y, y_onehot, is_training):
-        # x = x.view(x.size(0), -1)
 
         total_loss = 0.0
         for i, layer in enumerate(self.layers):
-            # print(x.shape, 'x.shape')
+
             if i == 0:
                 x = x.type(torch.cuda.FloatTensor)
                 # print(x.shape,'x.shape')
                 x = self.bn[i](x)
-            # print(x.shape, 'x.shape')
-            # print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
+
 
             x, loss = layer(x, y, y_onehot, i, is_training)
-            # print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
+
             total_loss += loss
-            # print(x.shape,'xrrrrrrrr',i)
 
         x = x.contiguous().view(x.size(0), -1)
         x = self.layer_out(x)
@@ -320,21 +276,12 @@ def to_one_hot(y, n_dims=None):
     return y_one_hot
 
 
-# input_ch,output_ch,height,num_layers,num_hiden,num_classes
-
 def train(train_loader, test_x_path, test_y_path,train_error,test_error,accuracy_list,epoch):
     model.train()
-    correct = 0
-    loss_total_local = 0
-    loss_total_global = 0
 
-    # total = sum([param.nelement() for param in model.parameters()])
-    # print('  + Number of params: %.3fM' % (total / 1e6),flops, params)
     total_num = sum(p.numel() for p in model.parameters())
     trainable_num = sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-    # for m in model.modules():
-    #     m.clear_stats()
 
     print('Total_Number of params: {} |Trainable_num of params: {} '.format(total_num, trainable_num))
     for step, (batch_x, batch_y) in enumerate(train_loader):
@@ -342,105 +289,62 @@ def train(train_loader, test_x_path, test_y_path,train_error,test_error,accuracy
         target_onehot = to_one_hot(batch_y)
         target_onehot = target_onehot.cuda()
 
-        # print(batch_x.shape,target_onehot.shape,batch_y.shape,'batch_x,target_onehot.shape,batch_y.shape')
-        # check_parameters(model,2)
-
         optimizer.zero_grad()
         output,loss= model(batch_x,batch_y, target_onehot,True)
-        loss_total_local += loss * batch_x.size(0)
 
         # print(output.shape,batch_y.shape,target_onehot.shape,'output.shape')
-        #
+       
         loss = loss_func (output, batch_y.long())
-        loss_total_global += loss.item() * batch_x.size(0)
-        # check_parameters(model, 2)
 
-
-        # loss_t.backward()
         loss.backward()
         optimizer.step()
-        # model.optim_step()
-    # loss_average_local = loss_total_local / len(train_loader.dataset)
-    # loss_average_global = loss_total_global / len(train_loader.dataset)
-    # error_percent = 100 - 100.0 * float(correct) / len(train_loader.dataset)
-    # string_print = 'Train epoch={},  loss_local={:.4f}, loss_global={:.4f}, error={:.3f}%, mem={:.0f}MiB, max_mem={:.0f}MiB\n'.format(
-    #     epoch,
-    #     loss_average_local,
-    #     loss_average_global,
-    #     error_percent,
-    #     torch.cuda.memory_allocated()/1e6,
-    #     torch.cuda.max_memory_allocated()/1e6)
-    # print(string_print)
-        # if epoch%10==0:
-        # print('局部更新')
-        # check_parameters(model, 2)
-    # check_parameters(model, 16)
-    # params = list(model.named_parameters())
-    # (name, param) = params[11]
-    # print('___________________________________________________________________\n', name, param,
-    #       '\n____________________________________________________________________')
-    if epoch % 6 == 0:
-        train_output = torch.max(output, 1)[1].cuda()
-        taccuracy = (torch.sum(train_output == batch_y.long()).type(torch.FloatTensor) / batch_y.size(0)).cuda()
+ 
+   
+       if epoch % 1 == 0:
+        model.eval()
 
+        test_x = np.load(test_x_path)
+        test_x_shape = test_x.shape
+        test_x = torch.from_numpy(np.reshape(test_x, [test_x_shape[0], 1, test_x_shape[1], test_x_shape[2]])).cuda()
 
-        error=1-taccuracy.item()
-        train_error.append(error)
-        print('epoch:',epoch,'accuracy:',taccuracy.item())
+        test_y = data_flat(np.load(pathlist[3]))
+        test_y = torch.from_numpy(test_y).cuda()
 
-        # np.save('./matplotlib_picture/UCI_error/predsim_train.npy', train_error)
+        test_y_onehot = to_one_hot(test_y)
+        test_y_onehot = test_y_onehot.cuda()
 
-    # if epoch % 1 == 0:
-    #     model.eval()
-    #
-    #     test_x = np.load(test_x_path)
-    #     test_x_shape = test_x.shape
-    #     test_x = torch.from_numpy(np.reshape(test_x, [test_x_shape[0], 1, test_x_shape[1], test_x_shape[2]])).cuda()
-    #
-    #     test_y = data_flat(np.load(pathlist[3]))
-    #     test_y = torch.from_numpy(test_y).cuda()
-    #
-    #     test_y_onehot = to_one_hot(test_y)
-    #     test_y_onehot = test_y_onehot.cuda()
-    #
-    #     # print(test_x.shape, test_y.shape, test_y_onehot.shape, 'test_x.shape,test_y.shape,target_y.shape')
-    #     # try:
-    #     test_output,_ = model(test_x,test_y,test_y_onehot,False )
-    #
-    #     test_output_copy = test_output
-    #     # print(test_output.shape,'test_output.shape')
-    #     test_output = data_flat(test_output.cpu().detach().numpy())
-    #     # print(test_output.shape, 'test_output')
-    #
-    #     test_output_f1 = np.asarray(pd.get_dummies(test_output))
-    #
-    #     print(test_y_onehot.shape, test_output_f1.shape)
-    #     acc = accuracy_score(test_y_onehot.cpu().numpy(), test_output_f1)
-    #     f1 = f1_score(test_y_onehot.cpu().numpy(), test_output_f1, average='weighted')
-    #     reca = recall_score(test_y_onehot.cpu().numpy(), test_output_f1, average='weighted')
-    #     label = [0,1, 2, 3, 4, 5]
-    #     # confu=confusion_matrix(test_y.cpu().numpy(), test_output,labels=label)
-    #     # print(confusion_y.tolist(), '\n', test_output_f1_con.tolist())
-    #     print('Epoch: ', epoch, '| test accuracy: %.8f' % acc, '| test F1: %.8f' % f1, '| test recall: %.8f' % reca)
-    #     #
-    #     test_output = torch.max(test_output_copy, 1)[1].cuda()
-    #     # print(test_output.shape,'test_output.shape')
-    #     accuracy = (torch.sum(test_output == test_y.long()).type(torch.FloatTensor) / test_y.size(0)).cuda()
-    #     # confu = confusion_matrix(test_y.cpu().numpy(), test_output.cpu(),labels=label)
-    #     # plt.matshow(confu, cmap=plt.cm.gray)
-    #     # print('Epoch: ', epoch, '| test accuracy: %.8f' % accuracy,'\n')
-    #     test_error.append((1-accuracy.item()))
-    #     accuracy_list.append(accuracy)
-    #     # except ValueError:
-    #     #     print('error')
-    #     # else:
-    #     #     pass
-    #     model.train()
-    # # plt.savefig('./confu.jpg')
-    #
-    # #
-    # # np.save('./accuracy.npy',accuracy_list)
-    # # np.save('./matplotlib_picture/alpha_dynamical/alpha-1.00.npy',test_error)
+        # print(test_x.shape, test_y.shape, test_y_onehot.shape, 'test_x.shape,test_y.shape,target_y.shape')
+        try:
+            test_output,_ = model(test_x,test_y, test_y_onehot,False)
+            test_output_copy = test_output
+            # print(test_output.shape)
+            test_output = data_flat(test_output.cpu().detach().numpy())
+            # print(test_output.shape, 'test_output')
+
+            test_output_f1 = np.asarray(pd.get_dummies(test_output))
+
+            print(test_y_onehot.shape, test_output_f1.shape)
+            acc = accuracy_score(test_y_onehot.cpu().numpy(), test_output_f1)
+            f1 = f1_score(test_y_onehot.cpu().numpy(), test_output_f1, average='weighted')
+            f2 = f1_score(test_y_onehot.cpu().numpy(), test_output_f1, average='micro')
+            f3 = f1_score(test_y_onehot.cpu().numpy(), test_output_f1, average='macro')
+            reca = recall_score(test_y_onehot.cpu().numpy(), test_output_f1, average='weighted')
+            # print(confusion_y.tolist(), '\n', test_output_f1_con.tolist())
+            print('Epoch: ', epoch, '| test accuracy: %.8f' % acc, '| test F1: %.8f' % f1, '| test recall: %.8f' % reca,
+                  '| test micro: %.8f' % f2, '| test micro: %.8f' % f3)
+            #
+            test_output = torch.max(test_output_copy, 1)[1].cuda()
+            # print(test_output.shape,'test_output.shape')
+            accuracy = (torch.sum(test_output == test_y.long()).type(torch.FloatTensor) / test_y.size(0)).cuda()
+            # print('Epoch: ', epoch, '| test accuracy: %.8f' % accuracy)
+            test_error.append((1 - accuracy.item()))
+            model.train()
+        except ValueError:
+            print('error')
+        else:
+            pass
+
+    # np.save('./matplotlib_picture/UCI_error/bp_test.npy',test_error)
 
 if __name__ == '__main__':
 
@@ -456,9 +360,9 @@ if __name__ == '__main__':
         train_error=[]
         test_error=[]
         accuracy_list=[]
-        lr=[0.004,0.001,0.0009,0.0007,0.0007]
-        lr = [0.001, 0.0008, 0.0007, 0.0006, 0.0003]
-        for epoch in range(3000):
+        lr=[0.004,0.001,0.0009,0.0007,0.0005]
+
+        for epoch in range(500):
             if epoch<=50:
                 lr_dynamic=lr[0]
                 model.set_learning_rate(lr_dynamic)
